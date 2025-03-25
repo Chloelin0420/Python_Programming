e9550f09d01169c1e9353c109adf9808c56edb69
Chloelin0420 <eahchiaoy@gmail.com>
2025-03-25 20:57:17 +0800

    Update ex2-2.py

--- ex2-2.py
+++ ex2-2.py
@@ -1,23 +1,12 @@
-...
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-...
 # this is my first program
 
-...
+'''
 ------------------------------
 this is hello python program
 ------------------------------
-...
 
-print ("hello") # this is my first program
+'''
+
+print("hello") # this is my first program
 
 # this is my first program
-
-print(2+3)
-
-print(9*9)
